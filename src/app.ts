65a73d8684a6e10e1f749e63d5bafcf7ac4ddce1
Augusto Hansel <augustohansel@hotmail.com>
2022-04-25 11:29:07 -0300

    feat: create login flow

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import express, {
+  json, NextFunction, Request, Response,
+} from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routers/index.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+app.use(router);
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error.type === 'validation error') {
+    return res.status(401).send(error.message);
+  }
+  if (error.type === 'not found') {
+    return res.status(404).send(error.message);
+  }
+  if (error.type === 'duplicate value') {
+    return res.status(409).send(error.message);
+  }
+  if (error.type === 'schema validate') {
+    return res.status(400).send(error.message);
+  }
+  if (error.type === 'declined') {
+    return res.status(405).send(error.message);
+  }
+  console.log(error);
+  return res.sendStatus(500);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
